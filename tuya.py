856ae15150169a8f9e8d586ddbbd26142269e875
reme2.718 <103392516+rem2718@users.noreply.github.com>
2023-08-08 01:47:06 +0300

    new

--- tuya.py
+++ tuya.py
@@ -2,6 +2,7 @@ import tinytuya
 import pymongo
 import asyncio
 import threading
+from bson import ObjectId
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
@@ -21,13 +22,42 @@ PASS = os.getenv("PASS")
 RECEIVER = [os.getenv("REM_EMAIL"), os.getenv("MAYA_EMAIL")]
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
-API_DEVICE = os.getenv("API_DEVICE")
+API_DEVICE = [os.getenv("API_DEVICE_10"), os.getenv("API_DEVICE_20"), os.getenv("API_DEVICE_30")]
 
 INTERVAL = timedelta(seconds=30)
 DAY = timedelta(hours=24)
 HALF_INTERVAL = INTERVAL/2
-DEVICES_SIZE = 3
-doc = {}
+DEVICES_SIZE = [8, 6, 9]
+
+docs = [{} * 3]
+
+ids = ['64d1548894895e0b4c1bc07f','64d154d494895e0b4c1bc081','64d154bc94895e0b4c1bc080']
+
+dev_map = {
+    'fridge_10': '64d160d293d44252699aa218',
+    'charger_10': '64d1609b93d44252699aa217',
+    'tv_10': '64d1605493d44252699aa216',
+    'fridge_11': '64d161e193d44252699aa219',
+    'food_processor_10': '64d15f9393d44252699aa215',
+    'lamp_10': '64d161fd93d44252699aa21a',
+    'office_strip_10': '64d1629393d44252699aa21b',
+    'tv_11': '64d162bf93d44252699aa21c',
+    'fridge_20': '64d1646b93d44252699aa221',
+    'coffee_maker_20': '64d1641a93d44252699aa220',
+    'lamp_20': '64d1648093d44252699aa222',
+    'charger_20': '64d162ff93d44252699aa21d',
+    'fan_20': '64d1638293d44252699aa21e',
+    'hair_dryer_20': '64d163dd93d44252699aa21f',
+    'cooler_30': '64d1656693d44252699aa225',
+    'toaster_30': '64d1685693d44252699aa22b',
+    'charger_30': '64d1682993d44252699aa22a',
+    'lamp_30': '64d1659d93d44252699aa226',
+    'air_fryer_30': '64d1650b93d44252699aa223',
+    'camera_30': '64d167a193d44252699aa228',
+    'speaker_30': '64d167e593d44252699aa229',
+    'tv_30': '64d1687493d44252699aa22c',
+    'office_strip_30': '64d165e693d44252699aa227',
+}
 
 
 def send_email(subject, body, receiver):
@@ -73,66 +103,69 @@ async def meross():
             if mid: 
                 for dev in meross_devices:
                     reading = await dev.async_get_instant_metrics()
-                    doc[dev.name] = reading.power
+                    docs[0][dev_map[dev.name]] = reading.power
             mid = not mid 
             prev_timestamp += HALF_INTERVAL              
     
 def get_pow():
-    cloud = tinytuya.Cloud(
+    cloud = [tinytuya.Cloud(
             apiRegion="eu", 
             apiKey=API_KEY, 
             apiSecret=API_SECRET, 
-            apiDeviceID=API_DEVICE)
-    tuya_devices = []
-    for dev in cloud.getdevices():
-        if dev['product_name'] != 'Smart Plug':
-            continue
-        tuya_devices.append(dev)
-        
+            apiDeviceID=api) for api in API_DEVICE]
+    
+    devices = [c.getdevices() for c in cloud]
+     
     prev_timestamp = datetime.now()
     mid = True
     while True:
         current_timestamp = datetime.now()
         if current_timestamp - prev_timestamp >= HALF_INTERVAL:
             if mid: 
-                for dev in tuya_devices:
-                    connected = cloud.getconnectstatus(dev['id'])
-                    if connected:
-                        result = cloud.getstatus(dev['id'])
-                        state = result['result'][4]['value']
-                        doc[dev['name']] = state/10.0
-                    else:
-                        doc[dev['name']] = None
+                for i in range(3):
+                    docs[i]['user'] = ObjectId(ids[i])
+                    for dev in devices[i]:
+                        connected = cloud[i].getconnectstatus(dev['id'])
+                        if connected:
+                            result = cloud[i].getstatus(dev['id'])
+                            state = result['result'][4]['value']
+                            docs[i][dev_map[dev['name']]] = state/10.0
+                        else:
+                            docs[i][dev_map[dev['name']]] = None
             mid = not mid  
             prev_timestamp += HALF_INTERVAL
                                 
 def validate():
-    if len(doc.keys()) == DEVICES_SIZE + 1:
-        for key, value in doc.items():
-            if key == 'timestamp':
-                continue
-            if not isinstance(value, (int, float)):
-                for email in RECEIVER:
-                    send_email('ERROR', 'Null values', email)
-                break
-    else:
-        for email in RECEIVER:
-            send_email('ERROR', 'One or more of the devices is missing', email)  
+    for i in range(3):
+        if len(docs[i].keys()) == DEVICES_SIZE[i] + 2:
+            for key, value in docs[i].items():
+                if key == 'timestamp' or key == 'user':
+                    continue
+                if not isinstance(value, (int, float)):
+                    for email in RECEIVER:
+                        send_email('ERROR', f'Null values for user {i}', email)
+                    break
+        else:
+            for email in RECEIVER:
+                send_email('ERROR', f'One or more of the devices is missing for user {i}', email)  
                  
 def insert_into_db():
     client = pymongo.MongoClient(URL)
     db = client['hemsproject']
-    collection = db['ourconsumption']
+    collection = db['PowerReadings']
     prev_timestamp = datetime.now()
     while True:
         current_timestamp = datetime.now()
         if current_timestamp - prev_timestamp >= INTERVAL:
             prev_timestamp += INTERVAL
-            doc['timestamp'] = prev_timestamp
-            collection.insert_one(doc)   
-            # validate()
-            print(f'{doc["timestamp"]}: done')
-            doc.clear()
+            for doc in docs:
+                doc['timestamp'] = prev_timestamp
+                
+            collection.insert_many(docs)   
+            validate()
+            print(f'{docs[0]["timestamp"]}: done')
+            for doc in docs:
+                doc.clear()
             
 
 pow_collector = threading.Thread(target=get_pow)
